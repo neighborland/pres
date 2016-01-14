39b048df7ebd0e2f12597809574c5d8b97202192
Tee Parham <tee@neighborland.com>
2016-01-14 13:00:58 -0700

    Fix view context delegation

--- lib/pres/view_delegation.rb
+++ lib/pres/view_delegation.rb
@@ -6,13 +6,17 @@ module Pres
       attr_reader :view_context
     end
 
-    # Send missing symbols to view_context
+    # Send missing symbols to view_context first
     def method_missing(symbol, *args, &block)
-      view_context.send(symbol, *args, &block)
+      if view_context.respond_to?(symbol, true)
+        view_context.send(symbol, *args, &block)
+      else
+        super
+      end
     end
 
     def respond_to_missing?(symbol, _ = false)
-      super || view_context.respond_to?(symbol, true)
+      view_context.respond_to?(symbol, true) || super
     end
   end
 end
