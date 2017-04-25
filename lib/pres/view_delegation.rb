320111aaa64f469aa97c3fac97e01e585071e263
Tee Parham <tee@neighborland.com>
2017-04-25 08:30:10 -0600

    Rename arguments to method

--- lib/pres/view_delegation.rb
+++ lib/pres/view_delegation.rb
@@ -4,17 +4,17 @@ module Pres
       @view_context
     end
 
-    # Send missing symbols to view_context first
-    def method_missing(symbol, *args, &block)
-      if view_context.respond_to?(symbol, true)
-        view_context.send(symbol, *args, &block)
+    # Send missing methods to view_context first
+    def method_missing(method, *args, &block)
+      if view_context.respond_to?(method, true)
+        view_context.send(method, *args, &block)
       else
         super
       end
     end
 
-    def respond_to_missing?(symbol, _ = false)
-      view_context.respond_to?(symbol, true) || super
+    def respond_to_missing?(method, _ = false)
+      view_context.respond_to?(method, true) || super
     end
   end
 end
